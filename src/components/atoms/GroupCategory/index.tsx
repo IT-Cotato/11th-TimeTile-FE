2e58c1bf0033c4d50591236727b60306519413a2
choyeon <cho1428hee@naver.com>
2025-07-08 18:25:19 +0900

    ui: GroupCategory

---
+++ src/components/atoms/GroupCategory/index.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { TagCategory } from "../TagCategory";
+import { TagCategoryName } from "@/model/common/tagcategory";
+import { Text } from "../Text";
+
+const FIRST_LINE: TagCategoryName[] = [
+  "콘서트/팬미팅",
+  "페스티벌/축제",
+  "시상식",
+  "앨범발매",
+  "쇼케이스",
+  "음악방송",
+];
+const SECOND_LINE: TagCategoryName[] = [
+  "유튜브",
+  "라디오",
+  "TV예능",
+  "드라마",
+  "영화",
+  "팬사인회/기타",
+  "미니팬미팅",
+];
+const THIRD_LINE: TagCategoryName[] = [
+  "화보/인터뷰",
+  "광고/모델",
+  "콜라보",
+  "라이브 방송",
+  "기타",
+];
+
+export const GroupCategory = () => {
+  const [selectedCategories, setSelectedCategories] = useState<
+    Set<TagCategoryName>
+  >(new Set());
+
+  const toggleCategory = (category: TagCategoryName) => {
+    setSelectedCategories((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(category)) newSet.delete(category);
+      else newSet.add(category);
+      return newSet;
+    });
+  };
+
+  const renderTag = (category: TagCategoryName) => (
+    <TagCategory
+      key={category}
+      category={category}
+      variant={selectedCategories.has(category) ? "select" : "default"}
+      onClick={() => toggleCategory(category)}
+    />
+  );
+
+  return (
+    <Container>
+      <Line>{FIRST_LINE.map(renderTag)}</Line>
+      <Line>{SECOND_LINE.map(renderTag)}</Line>
+      <Line>{THIRD_LINE.map(renderTag)}</Line>
+      <Padding />
+      {selectedCategories.size === 0 && (
+        <Text typo="Caption_4" color="warning">
+          활동 종류를 선택해주세요.
+        </Text>
+      )}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  align-content: flex-start;
+  align-self: stretch;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+const Line = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+const Padding = styled.div`
+  height: 16px;
+  width: 100%;
+`;
--- src/components/atoms/TagCategory/index.tsx
+++ src/components/atoms/TagCategory/index.tsx
@@ -1,35 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
+import { Text } from "../Text";
+import { TagCategoryName } from "@/model/common/tagcategory";
+import { TAG_CATEGORY_STYLE_MAP } from "@/constants/atoms/tagCategory";
 import {
   TagVariantType,
   TagCategoryPropsType,
 } from "@/model/components/TagCategory";
-import { TAG_CATEGORY_STYLE_MAP } from "@/constants/atoms/tagCategory";
-import { Text } from "../Text";
 import { FlexBox } from "@/components/layouts/FlexBox";
-import { TagCategoryName } from "@/model/common/tagcategory";
 
 export const TagCategory = ({
   category,
-  variant: variantProp,
-  ...props
+  variant = "default",
+  onClick,
 }: TagCategoryPropsType) => {
-  const [variant, setVariant] = useState<TagVariantType>(
-    variantProp ?? "default"
-  );
-
-  // 클릭 시
-  const handleClick = () => {
-    setVariant((prev) => (prev === "default" ? "select" : "default"));
-  };
-
   return (
-    <FlexBox>
+    <FlexBox justify="center" align="center">
       <StyledTag
-        onClick={handleClick}
+        onClick={onClick}
         category={category}
         variant={variant}
-        {...props}
+        role="button"
+        tabIndex={0}
       >
         <Text typo="Caption_1">{category}</Text>
       </StyledTag>
@@ -41,16 +33,20 @@ const StyledTag = styled.span<{
   category: TagCategoryName;
   variant: TagVariantType;
 }>`
-  display: inline-flex;
-  padding: 4px 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 26px;
   border-radius: 13px;
+  box-sizing: border-box;
   color: ${({ category, variant }) =>
     TAG_CATEGORY_STYLE_MAP[category][variant].color};
+  background-color: ${({ category, variant }) =>
+    TAG_CATEGORY_STYLE_MAP[category][variant].background};
   border: ${({ category, variant }) =>
     variant === "select"
       ? `2px solid ${TAG_CATEGORY_STYLE_MAP[category][variant].color}`
       : `1px solid ${TAG_CATEGORY_STYLE_MAP[category][variant].color}`};
-  background-color: ${({ category, variant }) =>
-    TAG_CATEGORY_STYLE_MAP[category][variant].background};
+  padding: ${({ variant }) => (variant === "select" ? "3px 11px" : "4px 12px")};
   cursor: pointer;
 `;
