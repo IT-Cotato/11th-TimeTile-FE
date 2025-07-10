8aa7e9d7877d826ecf4a8caaabee82d0d9f3ef26
MINJU <mjryu00211@gmail.com>
2025-07-10 14:01:11 +0900

    Feat: ArtistProfileCard.stories.ts 구현

--- src/components/Timeline/ArtistProfileCard/ArtistProfileCard.stories.ts
+++ src/components/Timeline/ArtistProfileCard/ArtistProfileCard.stories.ts
@@ -1,10 +1,13 @@
-import { ArtistProfileCard } from './index'
 import type { Meta, StoryObj } from '@storybook/nextjs-vite'
+import { ArtistProfileCard } from './index'
 
 const meta: Meta<typeof ArtistProfileCard> = {
   title: 'Components/ArtistProfileCard',
   component: ArtistProfileCard,
   tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
 }
 
 export default meta
@@ -13,8 +16,8 @@ type Story = StoryObj<typeof ArtistProfileCard>
 export const Default: Story = {
   args: {
     artistName: '투모로우바이투게더',
-    followerCount: 21980,
+    followerCount: 21800,
     imageUrl: '/images/txt.jpg',
-    years: [2020, 2021, 2022],
+    years: [2020, 2021, 2022, 2023],
   },
 }
