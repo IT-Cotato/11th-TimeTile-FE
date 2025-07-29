f6327e9e46d6d7582b075cfa365a1a96b0718100
choyeon <cho1428hee@naver.com>
2025-07-29 17:29:37 +0900

    chore: 프록시 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://timetile-api.click/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -5,7 +5,7 @@ export const BASE_URL = "https://timetile-api.click/api";
 
 // 인증 필요 없는 요청 (ex: 로그인, 회원가입, 토큰 재발급)
 export const axiosApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -14,7 +14,7 @@ export const axiosApi = axios.create({
 
 // 인증이 필요한 요청 (accessToken 필요)
 export const authAxios = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
   withCredentials: true,
 });
 
