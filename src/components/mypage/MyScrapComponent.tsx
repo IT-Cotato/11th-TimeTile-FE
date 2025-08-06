84d3c4692aacff80669f2ce481969546a667835a
choyeon <cho1428hee@naver.com>
2025-08-06 13:17:59 +0900

    ui: 폴더 컴포넌트 생성

--- src/components/mypage/MyScrapComponent.tsx
+++ src/components/mypage/MyScrapComponent.tsx
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+import { ScrapFolder } from "./ScrapFolder";
+
 export const MyScrapComponent = () => {
-  return <>스크랩 컴포넌트</>;
+  return (
+    <Container>
+      <ScrapFolder name="에스파" count={105} />
+      <ScrapFolder name="투모로우바이투게더" count={105} />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  margin-top: 24px;
+  display: flex;
+  gap: 16px;
+`;
