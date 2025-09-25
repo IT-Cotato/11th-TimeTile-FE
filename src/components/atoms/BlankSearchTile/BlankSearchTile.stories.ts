a4f487291fcba9ade50041e97df37ae14b625146
choyeon <cho1428hee@naver.com>
2025-09-25 16:00:50 +0900

    chore: 스토리 작성 형식 변경

--- src/components/atoms/BlankSearchTile/BlankSearchTile.stories.ts
+++ src/components/atoms/BlankSearchTile/BlankSearchTile.stories.ts
@@ -1,13 +1,13 @@
 import { Meta, StoryObj } from "@storybook/nextjs-vite";
 import { BlankSearchTile } from ".";
 
-const meta: Meta<typeof BlankSearchTile> = {
+const meta = {
   title: "Atom/BlankSearchTile",
   component: BlankSearchTile,
   tags: ["autodocs"],
-};
+} satisfies Meta<typeof BlankSearchTile>;
 
 export default meta;
-type Story = StoryObj<typeof BlankSearchTile>;
+type Story = StoryObj<typeof meta>;
 
 export const BlankSearchTileStory: Story = {};
