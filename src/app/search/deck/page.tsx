6bbd82ebfc1199a429a317a4578277f343acdc3b
choyeon <cho1428hee@naver.com>
2025-09-30 15:21:26 +0900

    chore: 데크 검색 상세페이지 gap, width값 수정

--- src/app/search/deck/page.tsx
+++ src/app/search/deck/page.tsx
@@ -15,63 +15,6 @@ interface Artist {
   imageUrl: string;
 }
 
-export const mockDecks = [
-  {
-    id: "deck1",
-    name: "Bruno Mars",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f25dd",
-  },
-  {
-    id: "deck2",
-    name: "Taylor Swift",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f25aa",
-  },
-  {
-    id: "deck3",
-    name: "Adele",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f25bb",
-  },
-  {
-    id: "deck4",
-    name: "Ed Sheeran",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f25cc",
-  },
-  {
-    id: "deck5",
-    name: "BTS",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f25dd",
-  },
-  {
-    id: "deck6",
-    name: "BLACKPINK",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f25ee",
-  },
-  {
-    id: "deck7",
-    name: "Coldplay",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f25ff",
-  },
-  {
-    id: "deck8",
-    name: "Ariana Grande",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f2600",
-  },
-  {
-    id: "deck9",
-    name: "Maroon 5",
-    imageUrl:
-      "https://i.scdn.co/image/ab6761610000e5ebc36dd9eb55fb0db4911f2611",
-  },
-];
-
 export default function SearchDeckPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "";
@@ -80,14 +23,6 @@ export default function SearchDeckPage() {
   const [totalCount, setTotalCount] = useState(0);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-
-  //   useEffect(() => {
-  //     if (!query) return;
-
-  //     setArtists(mockDecks);
-  //     setTotalCount(mockDecks.length);
-  //     setTotalPages(Math.ceil(mockDecks.length / 8));
-  //   }, [query, page]);
 
   useEffect(() => {
     if (!query) return;
@@ -176,7 +111,9 @@ const PostWrapper = styled.div`
 const ProfileRow = styled.div`
   width: 1136px;
   display: flex;
+  align-items: flex-start;
+  align-content: flex-start;
+  gap: 16px 12px;
+  align-self: stretch;
   flex-wrap: wrap;
-  gap: 14px;
-  justify-content: flex-start;
 `;
