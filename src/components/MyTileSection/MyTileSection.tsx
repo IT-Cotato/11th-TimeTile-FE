d30d9d06a7ed23aad3e69b0a6022fff8c4021b84
JungHyunHann <jade20031229@gmail.com>
2025-10-08 16:37:15 +0900

    feat: 데크 탭 구현

---
+++ src/components/MyTileSection/MyTileSection.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { theme } from "@/styles/theme";
+import { Text } from "@/components/atoms/Text";
+import { TileCard } from "@/components/Timeline/TileCard";
+
+type TagVariant = "tile" | "deck" | "song";
+export interface Schedule {
+  title: string;
+  date: string;
+  tags: TagVariant[];
+  description: string;
+  thumbnails: string[];
+  thumbnailLabels: string[];
+  participants?: number;
+}
+
+export interface TimelineTile {
+  selected?: boolean;
+  schedules: Schedule[];
+}
+
+interface TimelineSectionProps {
+  month: number;
+  tiles: TimelineTile[];
+}
+
+export default function TimelineSection({
+  month,
+  tiles,
+}: TimelineSectionProps) {
+  return (
+    <Row>
+      <MonthCol>
+        <Text typo="Body_1" color="gray_1000">
+          {month}월
+        </Text>
+      </MonthCol>
+
+      <ContentCol>
+        <GroupCard>
+          {tiles.map((tile, i) => (
+            <TileCardRow key={i}>
+              <TileCard selected={!!tile.selected} schedules={tile.schedules} />
+            </TileCardRow>
+          ))}
+        </GroupCard>
+      </ContentCol>
+    </Row>
+  );
+}
+
+const Row = styled.section``;
+
+const MonthCol = styled.div``;
+
+const ContentCol = styled.div``;
+
+const GroupCard = styled.div``;
+
+const TileCardRow = styled.div``;
